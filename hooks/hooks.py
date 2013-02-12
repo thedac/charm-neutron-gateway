4c81d040b0e47fca5b26de48a19de38ea611d20e
James Page <james.page@canonical.com>
2013-02-12 12:28:55 +0000

    Added hook for cluster-relation-changed

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -284,7 +284,8 @@ utils.do_hooks({
     "amqp-relation-joined": amqp_joined,
     "amqp-relation-changed": amqp_changed,
     "quantum-network-service-relation-changed": nm_changed,
-    "ha-relation-joined": ha_relation_joined
+    "ha-relation-joined": ha_relation_joined,
+    "cluster-relation-changed": cluster_changed,
     })
 
 sys.exit(0)
