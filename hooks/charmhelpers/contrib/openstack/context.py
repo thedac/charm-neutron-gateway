1adc0ff781653aaf361b31320f85d9b3576ad6f3
Liam Young <liam.young@canonical.com>
2014-09-10 17:18:56 +0000

    Add support for notifications with zmq

--- hooks/charmhelpers/contrib/openstack/context.py
+++ hooks/charmhelpers/contrib/openstack/context.py
@@ -50,6 +50,9 @@ from charmhelpers.contrib.network.ip import (
     get_ipv6_addr,
 )
 
+from charmhelpers.contrib.openstack.utils import (
+    get_matchmaker_map,
+)
 CA_CERT_PATH = '/usr/local/share/ca-certificates/keystone_juju_ca_cert.crt'
 
 
@@ -800,4 +803,25 @@ class ZeroMQContext(OSContextGenerator):
                     for unit in related_units(rid):
                         ctxt['zmq_nonce'] = relation_get('nonce', unit, rid)
                         ctxt['zmq_host'] = relation_get('host', unit, rid)
+        return ctxt
+
+class NotificationDriverContext(OSContextGenerator):
+
+    def __init__(self, zmq_relation='zeromq-configuration', amqp_relation='amqp'):
+        """
+        :param zmq_relation   : Name of Zeromq relation to check
+        """
+        self.zmq_relation = zmq_relation
+        self.amqp_relation = amqp_relation
+
+    def __call__(self):
+        ctxt = {
+            'notifications': "False",
+        }
+        if is_relation_made(self.zmq_relation):
+            matchmaker_data = get_matchmaker_map()
+            if 'notifications-info' in matchmaker_data:
+                ctxt['notifications'] = "True"
+        elif is_relation_made(self.amqp_relation):
+            ctxt['notifications'] = "True"
         return ctxt
--- hooks/charmhelpers/contrib/openstack/utils.py
+++ hooks/charmhelpers/contrib/openstack/utils.py
@@ -3,15 +3,17 @@
 # Common python helper functions used for OpenStack charms.
 from collections import OrderedDict
 
-import subprocess
+import json
 import os
 import socket
+import subprocess
 import sys
 
 from charmhelpers.core.hookenv import (
     config,
     log as juju_log,
     charm_dir,
+    is_relation_made,
     ERROR,
     INFO
 )
@@ -457,3 +459,11 @@ def get_hostname(address, fqdn=True):
             return result
     else:
         return result.split('.')[0]
+
+
+def get_matchmaker_map(mm_file='/etc/oslo/matchmaker_ring.json'):
+    mm_map = {}
+    if os.path.isfile(mm_file):
+        with open(mm_file, 'r') as f:
+            mm_map = json.load(f)
+    return mm_map
