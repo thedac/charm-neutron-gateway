3ccb2021361b724edce273994b5a95710bfcf2b4
David Ames <david.ames@canonical.com>
2015-09-21 11:25:31 -0700

    Specifically call config_changed which has the restart_on_change decorator

--- actions/openstack_upgrade.py
+++ actions/openstack_upgrade.py
@@ -13,6 +13,7 @@ from neutron_utils import (
     get_common_package,
 )
 
+from neutron_hooks import config_changed
 
 CONFIGS = register_configs()
 
@@ -25,9 +26,10 @@ def openstack_upgrade():
     code to run, otherwise a full service level upgrade will fire
     on config-changed."""
 
-    do_action_openstack_upgrade(get_common_package(),
+    if do_action_openstack_upgrade(get_common_package(),
                                 do_openstack_upgrade,
-                                CONFIGS)
+                                CONFIGS):
+        config_changed()
 
 
 if __name__ == '__main__':
